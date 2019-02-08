2dd2de3a9caa9a5c23bdddd6f047a0cd5e273bb5
David Klann <dklann@linux.com>
2019-02-08 14:46:18 -0600

    Comment style; add some diagnostic print statements.

--- src/commands/punch.py
+++ src/commands/punch.py
@@ -5,27 +5,36 @@ from wpilib.command import Command
 class Punch(Command):
     def __init__(self, robot):
         super().__init__()
-        
+
         self.robot = robot
         self.requires(self.robot.puncher)
 
     def initialize(self):
-        """Called just before this Command runs the first time"""
+        '''Called just before this Command runs the first time
+        '''
         self.robot.puncher.open()
+        print("Punch:initialize()")
 
     def execute(self):
-        """Called repeatedly when this Command is scheduled to run"""
+        '''Called repeatedly when this Command is scheduled to run
+        '''
         self.robot.puncher.open()
+        print("Punch:execute")
 
     def isFinished(self):
-        """Make this return true when this Command no longer needs to run execute()"""
+        '''Make this return true when this Command no longer needs to run execute()
+        '''
+        print("Punch:isFinished")
         return self.isTimedOut()
 
     def end(self):
-        """Called once after isFinished returns true"""
+        '''Called once after isFinished returns true
+        '''
         self.robot.puncher.close()
+        print("Punch:end")
 
     def interrupted(self):
-        """Called when another Command which requires one or more of the same
-           subsystems is scheduled to run"""
+        '''Called when another Command which requires one or more of the same
+        subsystems is scheduled to run
+        '''
         self.end()
