a6fe0e519cc8cc59b51d72520e4de9719f653d55
David Klann <dklann@linux.com>
2019-02-08 14:48:35 -0600

    Add placeholders for the various mode methods.

--- src/robot.py
+++ src/robot.py
@@ -5,27 +5,46 @@ from commandbased import CommandBasedRobot
 from wpilib.command import Scheduler
 from oi import OI
 from subsystems.drivetrain import DriveTrain
-from subsystems.arm import Arm
-#from commands.change_arm import MoveArm
-"""from subsystems.arm import arm
-from subsystems.claw import claw
-from subsystems.elevator import elevator
-from subsystems.hatch import hatch
-from subsystems.intake import intake
-from subsystems.puncher import puncher"""
+from subsystems.puncher import Puncher
+#from .subsystems.arm import Arm
+#from .subsystems.arm import Arm
+#from .subsystems.claw import Claw
+#from .subsystems.elevator import Elevator
+#from .subsystems.hatch import Hatch
+#from .subsystems.intake import Intake
+#from .commands.change_arm import MoveArm
 
 class MyRobot(CommandBasedRobot):
     #Our robot is pieced together in this class.
 
     def robotInit(self):
         self.drivetrain = DriveTrain(self)
+        self.puncher = Puncher(self)
         self.oi = OI(self)
         #self.arm = Arm(self)
-    
+
+    def disabledInit(self):
+        return super().disabledInit()
+
+    def disabledPeriodic(self):
+        return super().disabledPeriodic()
+
+    def autonomousInit(self):
+        return super().autonomousInit()
+
+    def autonomousPeriodic(self):
+        Scheduler.getInstance().run()
+
     def teleopInit(self):
-        pass
+        return super().teleopInit()
 
     def teleopPeriodic(self):
+        Scheduler.getInstance().run()
+
+    def testInit(self):
+        return super().testInit()
+
+    def testPeriodic(self):
         Scheduler.getInstance().run()
 
 if __name__ == "__main__":
