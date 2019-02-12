9739d6b0f03f9814b55c925d0d6d1cd7e937a69b
Jonah Jepsen <jonah.jepsen@gmail.com>
2019-02-12 17:08:51 -0600

    I'm only committing this so I'm able to merge

--- src/commands/move_arm_with_triggers.py
+++ src/commands/move_arm_with_triggers.py
@@ -12,23 +12,25 @@ class MoveArmWithTriggers(Command):
     def initialize(self):
         #Called just before this Command runs the first time
         self.robot.arm.move()
+        print ("arm initialize")
 
     def execute(self):
         #Called repeatedly when this Command is scheduled to run
         self.robot.arm.move()
+        print ("arm execute")
 
     def isFinished(self):
-        '''Make this return true when this Command no longer needs to run execute()
-        '''
-        print("Punch:isFinished")
+        #Make this return true when this Command no longer needs to run execute()
+        print ("arm isFinished")
         return self.isTimedOut()
 
     def end(self):
         #Called once after isFinished returns true
-        pass
+        print ("arm end")
 
     def interrupted(self):
         '''Called when another Command which requires one or more of the same
         subsystems is scheduled to run
         '''
+        print ("arm interrupted")
         self.end()
