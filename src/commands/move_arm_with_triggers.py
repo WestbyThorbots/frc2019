01b6c001a75172e7605e31d08401290df91c320b
David Klann <dklann@linux.com>
2019-02-15 08:31:52 -0600

    Refactor the Arm.
    Move the Xbox stuff out of the subsystem, and into the command.
    
    Add a global .pylintrc for linting.

--- src/commands/move_arm_with_triggers.py
+++ src/commands/move_arm_with_triggers.py
@@ -2,6 +2,8 @@
 '''Move the arm.'''
 
 from wpilib.command import Command
+from wpilib import XboxController
+from wpilib.interfaces.generichid import GenericHID
 
 class MoveArmWithTriggers(Command):
     '''Move the arm.'''
@@ -11,29 +13,30 @@ class MoveArmWithTriggers(Command):
 
         self.robot = robot
         self.requires(self.robot.arm)
+        self.xbox0 = XboxController(0)
 
     def initialize(self):
         #Called just before this Command runs the first time
-        self.robot.arm.move()
-        print ("arm initialize")
+        print("arm initialize")
 
     def execute(self):
         #Called repeatedly when this Command is scheduled to run
-        self.robot.arm.move()
-        print ("arm execute")
+        self.robot.arm.move(self.xbox0.getTriggerAxis(GenericHID.Hand.kLeft) +
+                            self.xbox0.getTriggerAxis(GenericHID.Hand.kRight)*-1)
+        print("arm execute")
 
     def isFinished(self):
         #Make this return true when this Command no longer needs to run execute()
-        print ("arm isFinished")
+        print("arm isFinished")
         return self.isTimedOut()
 
     def end(self):
         #Called once after isFinished returns true
-        print ("arm end")
+        print("arm end")
 
     def interrupted(self):
         '''Called when another Command which requires one or more of the same
         subsystems is scheduled to run
         '''
-        print ("arm interrupted")
+        print("arm interrupted")
         self.end()
