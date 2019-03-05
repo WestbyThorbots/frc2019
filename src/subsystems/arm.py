6bf2c7310c99cc518f6131b9278e9253e2052a48
David Klann <dklann@linux.com>
2019-03-04 19:47:00 -0600

    New motor and encoder on the arm.

--- src/subsystems/arm.py
+++ src/subsystems/arm.py
@@ -14,24 +14,15 @@ class Arm(Subsystem):
 
         self.arm = ctre.WPI_TalonSRX(11)
         self.armencoder = Encoder(0, 1)
-        self.armencoder.setDistancePerPulse(0.14)
+        self.armencoder.setDistancePerPulse(0.07)
 
     def move(self, value):
         """Move the arm according to the left and right Xbox
         controller triggers."""
-        if self.armencoder.getDistance() < 40:
+        if self.armencoder.getDistance() < 120:
             self.arm.set(value)
-            if value > 0:
-                direction = "up"
-            elif value < 0:
-                direction = "down"
-            else:
-                direction = "stopped"
-            print("Arm moving", direction, "at", value)
-
-        print ("Arm angle is " + "%3f" % self.armencoder.getDistance())
 
     def stop(self):
         """Stop the arm."""
         self.arm.set(0.0)
-        #TODO: Change 0.0 to a value that holds the arm at its current value so it doesn't fall.
+        # TODO: Change 0.0 to a value that holds the arm at its current value so it doesn't fall.
