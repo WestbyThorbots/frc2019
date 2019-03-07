b784a2006511eb17a033f5ea7c1ce853a2efadf3
David Klann <dklann@linux.com>
2019-03-06 18:40:06 -0600

    Add missing command files.

---
+++ src/commands/eject_cargo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''Snag some cargo.'''
+
+from wpilib.command import Command
+
+class EjectCargo(Command):
+    '''Snag some cargo.'''
+    def __init__(self, robot):
+        '''Save the robot object and pull in the intake subsystem.'''
+        super().__init__()
+
+        self.robot = robot
+        self.requires(self.robot.intake)
+
+    def initialize(self):
+        """Called just before this Command runs the first time"""
+        self.robot.intake.eject()
+
+    def execute(self):
+        """Called repeatedly when this Command is scheduled to run."""
+        self.robot.intake.eject()
+
+    def isFinished(self):
+        """Make this return true when this Command no longer needs to
+        run execute()"""
+        return self.isTimedOut()
+
+    def end(self):
+        """Called once after isFinished returns true"""
+        self.robot.intake.stop()
+
+    def interrupted(self):
+        """Called when another command which requires one or more of
+        the same subsystems is scheduled to run"""
+        self.end()
