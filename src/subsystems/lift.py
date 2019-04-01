052c8e88241e6c40fd9b41572e44814e9d20f4e7
Jonah Jepsen <jonah.jepsen@gmail.com>
2019-04-01 18:47:26 -0500

    Incomplete but merged lift code

--- src/subsystems/lift.py
+++ src/subsystems/lift.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
-'''Manipulate the rear cylinder used for lifting onto the Hab.'''
-
 import wpilib
 from wpilib import DoubleSolenoid
 from wpilib.command import Subsystem
 
-class RearLift(Subsystem):
-    """Rear pneumatic lift"""
+class Lift(Subsystem):
     def __init__(self):
         super().__init__()
 
+        self.front_lift = DoubleSolenoid(0,6,7)
         self.rear_lift = wpilib.DoubleSolenoid(0,3,2)
 
-    def extend(self):
-        """Extend the cylinder to lift the robot."""
-        self.rear_lift.set(DoubleSolenoid.Value.kReverse)
+    def lift_front(self):
+        self.front_lift.set(DoubleSolenoid.Value.kReverse)
 
-    def retract(self):
-        """Retract the cylinder to lower the robot."""
+    def lift_rear(self):
         self.rear_lift.set(DoubleSolenoid.Value.kForward)
 
     def stop(self):
